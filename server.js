c9e1598cf02c6e537f4e8f43cb65978855791f7d
Fernando Guillen <fguillen.mail@gmail.com>
2017-03-12 13:02:24 +0100

    Create data.json if not exists

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const path = require('path');
 
 const app = express();
 
+
+if(!fs.existsSync('data.json')) {
+  fs.createReadStream('data.json.example').pipe(fs.createWriteStream('data.json'));
+};
+
 const DATA_FILE = path.join(__dirname, 'data.json');
 
 app.set('port', (process.env.PORT || 3000));
